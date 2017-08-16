37e7711627d86bc393c309fa80d44e4d1070e853
Wose <Jacksonteather@hotmail.com>
2017-08-16 14:53:00 -0400

    ⚡️ Generate map module, service, barrel; import service

---
+++ src/app/map/index.ts
@@ -0,0 +1,3 @@
+export * from './map.service';
+
+export { MapModule } from './map.module';
---
+++ src/app/map/map.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MapService } from './';
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: [
+
+  ],
+  providers: [
+    MapService,
+  ],
+})
+export class MapModule { }
