75dc85f9fc2df16f7801190dfc46c8f08bc1ac37
Wose <Jacksonteather@hotmail.com>
2017-08-25 12:05:09 -0400

    ⚡️ Add 'select all' method to filters with indeterminate state

--- src/app/map/filter/filter-control/filter-control.component.ts
+++ src/app/map/filter/filter-control/filter-control.component.ts
@@ -1,9 +1,16 @@
 import {
   Component,
   OnInit,
+  AfterViewInit,
   Input,
 } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+
+import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/filter';
+
+import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 
 import { FilterControl } from '.';
 
@@ -13,13 +20,50 @@ import { FilterControl } from '.';
   templateUrl: './filter-control.component.html',
   styleUrls: ['./filter-control.component.scss'],
 })
-export class FilterControlComponent implements OnInit {
+@AutoUnsubscribe()
+export class FilterControlComponent implements OnInit, AfterViewInit {
   @Input() filter: FilterControl;
   @Input() form: FormGroup;
 
-  constructor() { }
+  private filterSubscription: Subscription;
+
+  private checkedSubject = new Subject<boolean>();
+  checked = this.checkedSubject.asObservable();
+  private indeterminateSubject = new Subject<boolean>();
+  indeterminate = this.indeterminateSubject.asObservable();
+
+  get formControl() {
+    return this.form.get(this.filter.name);
+  }
 
   ngOnInit() {
+    this.filterSubscription = this.formControl.valueChanges
+      .filter(value => value && value instanceof Array)
+      .subscribe(value => this.updateCheckboxState(value));
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => this.updateCheckboxState(this.formControl.value))
+  }
+
+  select(event: Event) {
+    const newValue = (<HTMLInputElement>event.target).checked ?
+      [...this.filter.options.map(f => f[this.filter.value])] :
+      [];
+    this.formControl.setValue(newValue);
+  }
+
+  private updateCheckboxState(value: any[]): void {
+    if (value.length === this.filter.options.length) {
+      this.indeterminateSubject.next(false);
+      this.checkedSubject.next(true);
+    } else if (value.length > 0) {
+      this.checkedSubject.next(false);
+      this.indeterminateSubject.next(true);
+    } else {
+      this.indeterminateSubject.next(false);
+      this.checkedSubject.next(false);
+    }
   }
 
 }
